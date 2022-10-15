88841735cd4cc27ecf980f2245e7b32c003d8b5e
Zaytsevie <115174587+Zaytsevie@users.noreply.github.com>
2022-10-15 19:18:53 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// string
+const name = "Igor"
+console.log("name", name, typeof name)
+
+// number
+const age = 37
+console.log("age", age, typeof age)
+
+// boolean
+const russian = true
+console.log("russian", russian, typeof russian)
+
+// null
+const hungry = null
+console.log("hungry", hungry, typeof hungry)
+
+// undefined
+let liveIn
+console.log("liveIn", liveIn, typeof liveIn)
+
+// object
+const myInfo = {
+    myName: "Igor",
+    myAge: 37,
+    iAmRussian: true,
+}
+console.log("myInfo", myInfo, typeof myInfo)
+
+// symbol
+const id = Symbol("id")
+console.log("id", id, typeof id)
+
+// bigint :))) smile
+const howMuchMoneyIneed = 10000000000n
+console.log("howMuchMoneyIneed", howMuchMoneyIneed, typeof howMuchMoneyIneed)
